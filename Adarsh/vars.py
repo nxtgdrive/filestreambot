fcdbf14890e74c0856dde431fca4c56d582522cc
Shinigamiitg <101014455+Shinigamiitg@users.noreply.github.com>
2022-12-16 09:37:48 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -37,4 +37,4 @@ class Var(object):
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', ' postgres://wbnbfcqn:02VPtEWump_E3ok0wUSQLOugkf9TmhbD@peanut.db.elephantsql.com/wbnbfcqn'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", None)).split())) 
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
